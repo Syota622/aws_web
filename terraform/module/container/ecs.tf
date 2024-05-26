fccf5222d3341ef1d64ce3ac6fd21f37243ab2dc
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-05-26 17:06:48 +0900

    Update Terraform

--- terraform/module/container/ecs.tf
+++ terraform/module/container/ecs.tf
@@ -75,23 +75,9 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    # security_groups = [aws_security_group.alb_sg.id]
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-  ingress {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
-    # security_groups = [aws_security_group.alb_sg.id]
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
     # security_groups = [aws_security_group.alb_sg.id]
     cidr_blocks     = ["0.0.0.0/0"]
   }
