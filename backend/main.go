30ded6e0c036d6843be6610b92db65a4e8009184
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-05-31 22:20:46 +0900

    デバッグ

--- backend/main.go
+++ backend/main.go
@@ -13,8 +13,21 @@ import (
 )
 
 func main() {
-	// 環境変数からデータベース接続情報を取得
-	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	// 環境変数の読み込み
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// デバッグ用に環境変数をログに出力
+	log.Printf("DB_USER: %s, DB_PASSWORD: %s, DB_HOST: %s, DB_PORT: %s, DB_NAME: %s", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// DSN (Data Source Name) 文字列の生成
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+	// // 環境変数からデータベース接続情報を取得
+	// dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
 
 	// データベースに接続
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
