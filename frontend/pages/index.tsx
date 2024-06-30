52439f86368986e1b182c96556ed3b44a3a9d6d4
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-06-30 14:19:43 +0900

    GraphQLのAPI実行が成功

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,50 +1,45 @@
-// import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
-// const client = new ApolloClient({
-//   uri: process.env.NEXT_PUBLIC_API_URL,
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_API_URL}/query`,
+  cache: new InMemoryCache(),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
-// const HELLO_QUERY = gql`
-//   query {
-//     hello
-//     greet(name: "John")
-//   }
-// `;
+const HELLO_QUERY = gql`
+  query {
+    hello
+    greet(name: "John")
+  }
+`;
 
-// interface HelloQueryResult {
-//   hello: string;
-//   greet: string;
-// }
+interface HelloQueryResult {
+  hello: string;
+  greet: string;
+}
 
-// export default function Home({ data }: { data: HelloQueryResult }) {
-//   return (
-//     <div>
-//       <h1>GraphQL API Response</h1>
-//       <p>Hello: {data.hello}</p>
-//       <p>Greet: {data.greet}</p>
-//     </div>
-//   );
-// }
-
-// export async function getStaticProps() {
-//   const { data } = await client.query<HelloQueryResult>({
-//     query: HELLO_QUERY,
-//   });
-
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// }
-
-export default function Home() {
+export default function Home({ data }: { data: HelloQueryResult }) {
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
       <p>This is a simple React component.</p>
       <p>API URL: {process.env.NEXT_PUBLIC_API_URL}</p>
+      <p>Hello: {data.hello}</p>
+      <p>Greet: {data.greet}</p>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { data } = await client.query<HelloQueryResult>({
+    query: HELLO_QUERY,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
