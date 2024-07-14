c742e306b39de910c1f65ffebaffb421d2a5b85c
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-07-14 12:04:25 +0900

    ヘルスチェック追加

--- terraform/module/backend/alb.tf
+++ terraform/module/backend/alb.tf
@@ -29,7 +29,7 @@ resource "aws_lb_target_group" "ecs_tg" {
     unhealthy_threshold = 3
     timeout             = 5
     interval            = 30
-    path                = "/"
+    path                = "/graphiql"
     protocol            = "HTTP"
     matcher             = "200-299"
   }
