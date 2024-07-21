0f42582d7a881ef9c80d79d2c75d30ae742e6351
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-07-21 19:18:44 +0900

    修正

--- backend/main.go
+++ backend/main.go
@@ -70,11 +70,6 @@ func main() {
 		log.Fatalf("データベース接続に失敗しました: %v", err)
 	}
 
-	// // マイグレーションの実行
-	// if err := migrations.MigrateDB(db); err != nil {
-	// 	log.Fatalf("マイグレーションに失敗しました: %v", err)
-	// }
-
 	// 通常のAutoMigrate
 	if err := db.AutoMigrate(&models.User{}); err != nil {
 		log.Fatalf("AutoMigrateに失敗しました: %v", err)
