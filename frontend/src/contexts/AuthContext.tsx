3a095f2f15d7dc933b78498dd25d69444f43cb35
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-08-14 10:16:53 +0900

    コメントを削除

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -14,34 +14,25 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 // 認証状態を管理し、子コンポーネントに提供
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  // isAuthenticatedの状態とそれを更新する関数を定義
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   // コンポーネントがマウントされたときに実行される副作用
   useEffect(() => {
-    // ローカルストレージからauthTokenを取得
     const token = localStorage.getItem('authToken');
-    // トークンの有無に基づいて認証状態を設定
     setIsAuthenticated(!!token);
   }, []); // 空の依存配列は、この効果がコンポーネントのマウント時にのみ実行されることを意味
 
   // ログイン関数を定義
   const login = (token: string) => {
-    // トークンをローカルストレージに保存
     localStorage.setItem('authToken', token);
-    // 認証状態をtrueに設定
     setIsAuthenticated(true);
-    // ホームページにリダイレクト
     window.location.href = '/';
   };
 
   // ログアウト関数を定義
   const logout = () => {
-    // ローカルストレージからトークンを削除
     localStorage.removeItem('authToken');
-    // 認証状態をfalseに設定
     setIsAuthenticated(false);
-    // ログインページにリダイレクト
     window.location.href = '/login';
   };
 
