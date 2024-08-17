d81a25da58dc252fff08ebd686b250723153cbe6
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-08-17 15:54:28 +0900

    cognito id を追加

--- backend/db/migrations/000001_create_users_table.up.sql
+++ backend/db/migrations/000001_create_users_table.up.sql
@@ -1,11 +1,9 @@
 CREATE TABLE users (
     id VARCHAR(36) PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    created_at DATETIME(3) NOT NULL,
-    updated_at DATETIME(3) NOT NULL,
-    deleted_at DATETIME(3),
-    UNIQUE INDEX idx_username (username),
-    UNIQUE INDEX idx_email (email),
-    INDEX idx_deleted_at (deleted_at)
+    cognito_id VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+    deleted_at DATETIME(3)
 );
