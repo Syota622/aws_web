34727a4e3afa52320c6cabdd1d1fa122ed1bb8f1
Syota0622 <wvp360gqanukcqhg@gmail.com>
2024-09-13 15:02:22 +0900

    ホストヘッダーを修正

--- terraform/module/alb/listerner-rule.tf
+++ terraform/module/alb/listerner-rule.tf
@@ -1,3 +1,21 @@
+### リスナールール ###
+# Priority 100 Rule for api.mokokero.com
+resource "aws_lb_listener_rule" "api_mokokero_com_rule" {
+  listener_arn = aws_lb_listener.https_listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_ecs_tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["api.mokokero.com"]
+    }
+  }
+}
+
 # # ALBリスナールールの更新
 # resource "aws_lb_listener_rule" "blue_green" {
 #   listener_arn = aws_lb_listener.https_listener.arn
